fdef1a2c0e8e04e5c96d385d079c01dd0ffa5a62
Lobe Musonda <lobe350@gmail.com>
2018-08-29 22:08:34 +0200

    Sign in details sent to database

--- app/src/main/java/com/lobemusonda/schoolbustracker_driver/LoginActivity.java
+++ app/src/main/java/com/lobemusonda/schoolbustracker_driver/LoginActivity.java
@@ -2,36 +2,98 @@ package com.lobemusonda.schoolbustracker_driver;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "LoginActivity";
     private static final int ERROR_DIALOG_REQUEST = 9001;
 
+    private FirebaseAuth mAuth;
+
+    EditText mEditTextEmail, mEditTextPassword;
+    ProgressBar mProgressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
 
-        Button login = (Button) findViewById(R.id.login);
+        mEditTextEmail = findViewById(R.id.editTextEmail);
+        mEditTextPassword = findViewById(R.id.editTextPassword);
+        mProgressBar = findViewById(R.id.progressBar);
+        Button login = findViewById(R.id.login);
 
         if (isServicesOk()) {
             login.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Intent intent = new Intent(LoginActivity.this, MapActivity.class);
-                    startActivity(intent);
+                    userLogin();
                 }
             });
         }
+    }
+
+    private void userLogin() {
+        String email = mEditTextEmail.getText().toString().trim();
+        String password = mEditTextPassword.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            mEditTextEmail.setError("Email is required");
+            mEditTextEmail.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            mEditTextEmail.setError("Please enter a valid email");
+            mEditTextEmail.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            mEditTextPassword.setError("Password is required");
+            mEditTextPassword.requestFocus();
+            return;
+        }
+
+        if (password.length() < 6) {
+            mEditTextPassword.setError("Minimum length of password should be 6");
+            mEditTextPassword.requestFocus();
+            return;
+        }
+
+        mProgressBar.setVisibility(View.VISIBLE);
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                mProgressBar.setVisibility(View.GONE);
+                if (task.isSuccessful()) {
+                    Intent intent = new Intent(LoginActivity.this, MapActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     public boolean isServicesOk() {
--- app/src/main/java/com/lobemusonda/schoolbustracker_driver/SignUpActivity.java
+++ app/src/main/java/com/lobemusonda/schoolbustracker_driver/SignUpActivity.java
@@ -1,24 +1,132 @@
 package com.lobemusonda.schoolbustracker_driver;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUpActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase mDatabase;
+
+    EditText mEditTextEmail, mEditTextPassword, mEditTextFullName, mEditTextBusNo;
+    ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        Button signUp = (Button) findViewById(R.id.buttonSignUp);
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance();
+
+        mEditTextFullName = findViewById(R.id.editTextFullName);
+        mEditTextEmail = findViewById(R.id.editTextEmail);
+        mEditTextPassword = findViewById(R.id.editTextPassword);
+        mEditTextBusNo = findViewById(R.id.editTextBusNo);
+        mProgressBar = findViewById(R.id.progressBar);
+
+        Button signUp = findViewById(R.id.buttonSignUp);
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(SignUpActivity.this, MapActivity.class);
-                startActivity(intent);
+                registerUser();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (mAuth.getCurrentUser() != null) {
+//            Handle the already Logged in user
+
+        }
+    }
+
+    private void registerUser() {
+        final String fullName = mEditTextFullName.getText().toString().trim();
+        final String email = mEditTextEmail.getText().toString().trim();
+        String password = mEditTextPassword.getText().toString().trim();
+        final String busNo = mEditTextBusNo.getText().toString().trim();
+
+        if (fullName.isEmpty()) {
+            mEditTextFullName.setError("Full name is required");
+            mEditTextFullName.requestFocus();
+            return;
+        }
+
+        if (email.isEmpty()) {
+            mEditTextEmail.setError("Email is required");
+            mEditTextEmail.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            mEditTextEmail.setError("Please enter a valid email");
+            mEditTextEmail.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            mEditTextPassword.setError("Password is required");
+            mEditTextPassword.requestFocus();
+            return;
+        }
+
+        if (password.length() < 6) {
+            mEditTextPassword.setError("Minimum length of password should be 6");
+            mEditTextPassword.requestFocus();
+            return;
+        }
+
+        if (busNo.isEmpty()) {
+            mEditTextBusNo.setError("Bus number is required");
+            mEditTextBusNo.requestFocus();
+            return;
+        }
+
+        mProgressBar.setVisibility(View.VISIBLE);
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+//                    We store additional fields in firebase database
+                    Driver driver = new Driver(fullName, email, busNo);
+                    mDatabase.getReference("Drivers")
+                            .child(mAuth.getCurrentUser().getUid())
+                            .setValue(driver).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            mProgressBar.setVisibility(View.GONE);
+                            if (task.isSuccessful()) {
+                                Toast.makeText(getApplicationContext(), "Registration successful", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(SignUpActivity.this, MapActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(intent);
+                            } else {
+                                Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
